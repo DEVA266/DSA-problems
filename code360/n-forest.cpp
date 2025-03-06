916bd954c5c993dc97546d92fecee65baef597c1
DEVA266 <devats266@gmail.com>
2025-03-06 22:25:24 +0530

    mm

--- code360/n-forest.cpp
+++ code360/n-forest.cpp
@@ -1,8 +1,14 @@
-// the problem link in code 360
-//www.naukri.com/code360/problems/n-forest_6570177 //
+/* the problem link in code 360
+www.naukri.com/code360/problems/n-forest_6570177 */
 
-void nForest(int n) {
+//void nForest(int n) {
 	// Write your code here.
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
 	for ( int i = 0; i<n; i++){
 		for (int j=0; j<n; j++){
 			cout << "* ";
@@ -10,3 +16,6 @@ void nForest(int n) {
 		cout << endl;
 	}
 }
+
+
+
