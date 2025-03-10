7a33f1f4a1b9735d1ed525310f281ecb159a6bd6
DEVA266 <devats266@gmail.com>
2025-03-10 07:34:41 +0530

    changes added to it

--- STL.cpp
+++ STL.cpp
@@ -103,7 +103,95 @@ void explaindeque(){
     li.push_front(1); // {1,5,4}
     li.emplace_front(2); // {2,1,5,4}
 }
+void explainstack(){
+    stack<int> s;
+    s.push(1); // {1}
+    s.push(2); // {2,1}
+    s.emplace(6); // {6,2,1}
+    s.emplace(1); // {1,6,2,1}
+    // in stack it is called as last in first out method,the element that enters the stack last is the elemenet that gets out first
+    // random acces in stack is not possible
+    cout << s.top(); // 1
+    cout << s.pop(); // {6,2,1}
+    cout << s.top(); // 6
 
+}
+
+void explainqueue(){
+    queue<int> q; // first in first out like the queue concept in everywhere
+    q.push(2); // {2}
+    q.push(3); // {2,3}
+    q.push(6); // {2,3,6}
+    cout << q.front(); // 2
+    cout << q.back(); // 6
+    cout << q.pop(); // {3,6}
+    cout << q.front(); // 3
+}
+
+void explainpriority(){
+    priority_queue<int> p; // similar to the queue but it stores the largest element (most weightage) at the top of the queue
+    p.push(3); // {3}
+    p.push(2); //{3,2}
+    p.push(8); // {8,3,2}
+    cout << p.top(); // 8
+    cout << p.pop(); // {3,2}
+    cout << p.top(); // 3
+    // the above mentioned priority stores the max at the top called as max heap
+
+    // the code for min heap
+    priority_queue<int, vector<int>, greater<int> > pm;
+    pm.push(3); // {3}
+    pm.push(7); // {3,7}
+}
+
+void explainset(){
+    set<int> s;
+    s.inser(1); // {1}
+    s.insert(6); // {1,6}
+    s.insert(3); // {1,3,6}
+    s.insert(6); // {1,3,6}
+    // every set elements are unique no repeated values
+    // elements in the set are sorted in accending order
+
+    auto it = s.find(3); // it store the address of the value 3 in the set
+    auto it = s.find(8); // the element is not stored in the set so it return the s.end() value , which means the value right after the last value in the set
+
+    cout << s.find(3); // if the element prsent in the set it return true , if not false
+    s.erase(6); // {1,3}
+
+    // multi set is same as the one in the set but it also store the duplicates also 
+    multiset<int> ms;
+}
+
+void map(){
+    map<int , int > map;
+    map<int, pair<int,int> > mpp;
+    map<pair<int,int>,int> mpp;
+    // map stores the value in terms of the key , the first elemenet is consider as the key and the second elemenet is consider as the value
+    // the key in the map is unique and present in the sorted order
+    m[1] = 2; // {1,2}
+    m.insert(13,8); // { {1,2}, {13,8}}
+    m.emplace(2,6); // { {1,2}, {2,6}, {13,8}}
+    // to print the value in the map
+    for(auto i : m){
+        cout << i.first << " " << i.second << endl;
+    }   
+}
+
+// sort and pair in the descending order
+bool comp(pair<int,int> p1, pair<int,int> p2){
+    if(p1.second < p2.second) return true;
+    else return false;
+    if(p1.first > p2.first) return true;
+    else return false;
+}
+
+void sort(){
+    // to sort an array
+    sort(a,a+n); // it sorts the given array a , n is the total size of the array
+    // to sort an pair , where by taking the second elements , if the second element is same then sort the first element based on the descending order
+    sort(a,a+n,comp) ; // before sort {{3,2}, {2,1}, {4,1}} , after sorting {{4,1}, {2,1}, {3,2}}
+}
 int main(){
     int n;
     vector();
