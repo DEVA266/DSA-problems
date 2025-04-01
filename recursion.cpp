76395b6b5163149e1d188ab5428696e0cd6eec4b
DEVA266 <devats266@gmail.com>
2025-04-01 21:18:29 +0530

    added

--- recursion.cpp
+++ recursion.cpp
@@ -29,6 +29,54 @@ void print_name(){
     printGfg(N);
 }
 
+void sum_numbers(){
+    // link : https://www.geeksforgeeks.org/problems/sum-of-first-n-terms5843/1
+    int sum=0;
+    int sumOfSeries(int n) {
+        // code here
+        if(n>0){
+            sum += n*n*n ;
+            return sumOfSeries(n-1);
+        }
+    return sum ;
+    }
+
+    // method 2
+    int sumOfSeries(int n){
+        if (n==0){
+            return 0;
+        }
+        return (n*n*n)+sumOfSeries(n-1);
+    }
+}
+
+void sum(){
+    // returning sum of n numbers in a parameterized way
+    int sub(n,sum){
+        if(n<0){
+            cout << sum ;
+            return ;
+        }
+        sub(n-1,sum+n);
+    }
+
+    // the values to be passed for calling the function
+    /* 
+    int sum = 0 ;
+    int n ;
+    cin >> "enter the number " >> n ;
+    cout << "to find sum of n numbers ";
+    sub(n,sum) ;
+    */
+}
+
+void fact(){
+    int factorial(n){
+        if(n==0 || n==1 ) return 1 ;
+        else return n*factorial(n-1);
+    }
+}
+
 int main (){
 
 }
